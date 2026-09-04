ed111ed14567b724e657b23623b7750aae7db4a0
Mateo Echavarria <Teoechavarriagalvis@gmail.com>
2026-09-04 16:22:50 +0200

    Nuevos cammbios

--- calculadora.py
+++ calculadora.py
@@ -8,3 +8,11 @@ def restar(a, b):
 
 def multiplicar(a, b):
     return a * b
+
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir entre cero.")
+    return a / b
+
+def potencia(a, b):
+    return a ** b
